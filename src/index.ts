ded098af1709c91cb56bdd93b76830fe35f83f29
Oliver Emery <thrymgjol@gmail.com>
2019-09-27 16:31:54 -0500

    Refactored to danger plugin format.

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,79 @@
-import { danger, warn, TextDiff, message } from 'danger'
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
+import { TextDiff } from "../node_modules/danger/distribution/dsl/GitDSL"
+
+declare var danger: DangerDSLType
+
+export declare function warn(message: string): void
+export declare function fail(message: string): void
 
 interface PackageJSON {
-    dependencies: PackageMap
-    devDependencies: PackageMap
+  dependencies: PackageMap
+  devDependencies: PackageMap
 }
 
 interface PackageMap {
-    [key: string]: string
+  [key: string]: string
 }
 
-function getPackageAdditions(before: PackageMap, after: PackageMap): string[] {
-    const beforePackages = Object.keys(before)
-    const afterPackages = Object.keys(after)
+const addedPackages: string[] = []
+const addedDevPackages: string[] = []
 
-    return afterPackages.filter(pkg => !~beforePackages.indexOf(pkg))
+const addedDependenciesMessage = "Added dependencies"
+const addedDevDependenciesMessage = "Added dev dependencies"
+
+function getPackageAdditions(before: PackageMap, after: PackageMap): string[] {
+  const beforePackages = Object.keys(before)
+  const afterPackages = Object.keys(after)
+
+  return afterPackages.filter(pkg => !beforePackages.includes(pkg))
+}
+
+function populateAddedPackages(diff: TextDiff | null): void {
+  if (!diff) {
+    return
+  }
+
+  const before: PackageJSON = JSON.parse(diff.before)
+  const after: PackageJSON = JSON.parse(diff.after)
+
+  addedPackages.push(
+    ...getPackageAdditions(before.dependencies, after.dependencies)
+  )
+  addedDevPackages.push(
+    ...getPackageAdditions(before.devDependencies, after.devDependencies)
+  )
+}
+
+if (danger.git.modified_files.includes("package.json")) {
+  danger.git.diffForFile("package.json").then(populateAddedPackages)
 }
 
 function makePackageList(packages: string[]) {
-    return packages.reduce((list, dep) => `${list}\n + ${dep}`, '')
+  return packages.reduce((list, dep) => `${list}\n + ${dep}`, "")
 }
 
-function checkAddedPackages(diff: TextDiff | null): void {
-    if (!diff) {
-        return
-    }
-
-    const before: PackageJSON = JSON.parse(diff.before)
-    const after: PackageJSON = JSON.parse(diff.after)
-
-    const addedPackages = getPackageAdditions(before.dependencies, after.dependencies)
-    const addedDevPackages = getPackageAdditions(before.devDependencies, after.devDependencies)
-
-    if (addedPackages.length) {
-        warn('Added dependencies:' + makePackageList(addedPackages))
-    }
-
-    if (addedDevPackages.length) {
-        warn('Added dev dependencies:' + makePackageList(addedDevPackages))
-    }
+function notifyAddedPackages(
+  notifyFunction: (msg: string) => void,
+  message: string,
+  packageList: string[]
+) {
+  if (packageList.length) {
+    notifyFunction(message + ":" + makePackageList(packageList))
+  }
 }
 
-if (~danger.git.modified_files.indexOf('package.json')) {
-    danger.git.diffForFile('package.json')
-        .then(checkAddedPackages)
+export function warnAddedDependencies() {
+  notifyAddedPackages(warn, addedDependenciesMessage, addedPackages)
+}
+
+export function failAddedDependencies() {
+  notifyAddedPackages(fail, addedDependenciesMessage, addedPackages)
+}
+
+export function warnAddedDevDependencies() {
+  notifyAddedPackages(warn, addedDevDependenciesMessage, addedDevPackages)
+}
+
+export function failAddedDevDependencies() {
+  notifyAddedPackages(fail, addedDevDependenciesMessage, addedDevPackages)
 }
